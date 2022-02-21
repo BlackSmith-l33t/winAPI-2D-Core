744cf4902271b58d56078441924293ae43622fc0
Melchizedek <nobody133738@gmail.com>
2022-02-21 16:56:46 +0900

    start : spcace를 부르면 게임 시작 / ball 전진 가능

--- winAPI_2D_imitation/winAPI_2D_imitation/CCore.cpp
+++ winAPI_2D_imitation/winAPI_2D_imitation/CCore.cpp
@@ -1,9 +1,16 @@
 #include "framework.h"
 #include "CCore.h"
 #include "CGameObject.h"
+
 // CCore* CCore::_instance = NULL;
-CGameObject vaus;
-CGameObject ball;
+#define BALL_RADIUS 15
+
+CGameObject vaus;  // TODO : 바우스에 색깔넣기
+CGameObject ball;  // TODO : 공에 색깔넣기
+fPoint vausPos;
+fPoint ballPos;
+
+bool IsStart = false;
 
 CCore::CCore()
 {
@@ -22,12 +29,12 @@ CCore::~CCore()
 void CCore::update()
 {
 	CTimeManager::getInst()->update();
+	vausPos = vaus.GetPos();
+	ballPos = ball.GetPos();
 
-	fPoint vausPos = vaus.GetPos();
-	fPoint ballPos = ball.GetPos();
 	// 게임 정보 갱신 진행
 	// GetAsyncKeyState : 메시지 큐에 키 입력을 받는 방식이 아닌  현재 상태의 키 입력상태를 확인
-	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000) // 해당 키가 누려있는지 확인하는 코드
 	{
 		vausPos.x -= 500 * CTimeManager::getInst()->GetDT();
 	}
@@ -37,15 +44,10 @@ void CCore::update()
 		vausPos.x += 500 * CTimeManager::getInst()->GetDT();
 	}
 
-	/*if (GetAsyncKeyState(VK_UP) & 0x8000)
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) // Space를 누르면 게임이 시작됨.
 	{
-		pos.y -= 100 * CTimeManager::getInst()->GetDT();
+		IsStart = true;
 	}
-
-	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
-	{
-		pos.y += 100 * CTimeManager::getInst()->GetDT();
-	}*/
 
 	vaus.SetPos(vausPos);
 	ball.SetPos(ballPos);
@@ -55,6 +57,7 @@ void CCore::render()
 {	
 	// 게임 정보를 토대도 memDC에 그리기 작업 진행
 	Rectangle(m_hMemDC, -1, -1, WINSIZEX + 1, WINSIZEY + 1);
+	float ballSpeed = 0.2;
 	//Ellipse(m_hMemDC, -1, -1, WINSIZEX + 1, WINSIZEY + 1);
 
 	Rectangle(m_hMemDC,
@@ -68,6 +71,25 @@ void CCore::render()
 		ball.GetPos().y - ball.GetScale().y / 2,
 		ball.GetPos().x + ball.GetScale().x / 2,
 		ball.GetPos().y + ball.GetScale().y / 2);
+
+	RECT rc;
+	rc.left = 0;
+	rc.top = 0;
+	rc.right = WINSIZEX;
+	rc.bottom = WINSIZEY;
+		
+
+	if (true == IsStart)
+	{
+		if (ballPos.y >= rc.top + BALL_RADIUS / 2)
+		{
+			ballSpeed *= -1;
+		}
+		
+		ballPos.y += ballSpeed;
+
+		ball.SetPos(ballPos);
+	}
 
 
 
@@ -83,7 +105,7 @@ void CCore::render()
 void CCore::init()
 {
 	// 게임 초기화 작업 진행
-	CTimeManager::getInst()->init();
+	CTimeManager::getInst()->init();	
 	
 	// 게임 윈도우의 DC 핸들값 가져오기
 	m_hDC = GetDC(hWnd);	
@@ -96,5 +118,5 @@ void CCore::init()
 	DeleteObject(hOldBitmap);
 
 	vaus = CGameObject(fPoint(600, 690), fPoint{ 150, 25 });
-	ball = CGameObject(fPoint(600, 660), fPoint{ 15, 15 });
+	ball = CGameObject(fPoint(600, 660), fPoint{ BALL_RADIUS, BALL_RADIUS });
 }
