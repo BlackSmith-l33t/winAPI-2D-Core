502e6df32238ee97495fda4c45965458041e76b6
Melchizedek <nobody133738@gmail.com>
2022-02-26 14:29:13 +0900

    Resource 구성 완료 / Texture 리소스 추가

---
+++ winAPI_2D_imitation/winAPI_2D_imitation/CTexture.cpp
@@ -0,0 +1,62 @@
+#include "framework.h"
+#include "CTexture.h"
+
+CTexture::CTexture()
+{
+	m_hDC = 0;
+	m_hBMP = 0;
+	m_bmpInfo = { };
+}
+
+CTexture::~CTexture()
+{
+}
+
+HDC CTexture::GetDC()
+{
+	return m_hDC;
+}
+
+HBITMAP CTexture::GetBitmap()
+{
+	return m_hBMP;
+}
+
+BITMAP CTexture::GetBitInfo()
+{
+	return m_bmpInfo;
+}
+
+int CTexture::GetBMPWidth()
+{
+	return m_bmpInfo.bmWidth;
+}
+
+int CTexture::GetBMPHeight()
+{
+	return m_bmpInfo.bmHeight;
+}
+
+void CTexture::Load(const wstring& strFilePath)
+{
+	m_hBMP = (HBITMAP)LoadImage(
+		nullptr, // hInstance, nulltptr로 유지
+		strFilePath.c_str(),	// 파일의 경로, C Style의 문자열 
+		IMAGE_BITMAP, // 이미지 타입, 비트맵 이미지로 지정
+		0, 0,		 // 이미지의 XY 크기, 0을 주면 자동으로 이미지크기 설정
+		LR_CREATEDIBSECTION | LR_LOADFROMFILE); // 이미지의 속성
+
+	if (0 == m_hBMP)
+	{
+		assert(nullptr);
+	}
+
+	// 비트맵을 가지고 BMPInfo
+	m_hDC = CreateCompatibleDC(CCore::getInst()->GetMainDC());
+
+	// 비트맵과 DC 연결
+	HBITMAP hOldBitmap = (HBITMAP)SelectObject(m_hDC, m_hBMP);
+
+	// 비트맵 Info 추출
+	GetObject(m_hBMP, sizeof(BITMAP), &m_bmpInfo);
+}
